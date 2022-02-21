d1080be9569a0404b8e6b3041553cacdd5df1653
Tomas Vergara <tominacho@hotmail.com>
2022-02-21 13:17:58 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Font Awesome 6 Site Scraper
+
+**Simple scraping script to get icons and their category from fontawesome.com**
+Used to create category files for Font Awesome Gallery, a vscode extension that allows you to view and search icons directly from Visual Studio Code:
+https://github.com/TOMIVERGARA/vscode-fontawesome-gallery
+
+> This solution was created as the latest release of Font Awesome(v6) no longer includes a `metadata` folder, which contained useful information about the icons. This script aims to generate some of those missing files by getting the information from fontawesome's site.
+
+NOTICE!
+You should update the chromedriver accordingly.
---
+++ main.py
@@ -0,0 +1,75 @@
+from bs4 import BeautifulSoup
+import json
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service("./chromedriver")
+driver = webdriver.Chrome(service=service)
+
+startingUrl = "https://fontawesome.com/search?m=free"
+
+
+def getCategoryUrl(category):
+    template = 'https://fontawesome.com/search?m=free&c={}'
+    category = category.replace('_', '-').replace('0', '-')
+    return template.format(category)
+
+
+def getCategoryNameFromLiTag(liTag):
+    rawCategoryName = liTag.label.find('span', {
+        'class': 'text-capitalize'
+    }).text
+    return rawCategoryName.lower().replace(' ', '_').replace('_+_', '0')
+
+
+def getIconId(icon):
+    iconIdSpan = icon.button.span
+    return iconIdSpan.text
+
+
+# Opens the browser and gets all available categories.
+driver.get(startingUrl)
+categoriesSoup = BeautifulSoup(driver.page_source, 'html.parser')
+categoriesLi = categoriesSoup.find('ul', {
+    'class': 'icons-facets-group-categories'
+}).find_all('li')
+
+categoryList = []
+categoryRecords = {}
+iconsCounter = 0
+
+print('Getting all available categories:')
+for li in categoriesLi:
+    categoryName = getCategoryNameFromLiTag(li)
+    if (categoryName == "genders"):
+        categoryName = "gender"
+    categoryList.append(categoryName)
+    print('➥ ' + categoryName)
+
+for category in categoryList:
+    print('Redirecting to ' + category + ' icons page...')
+    driver.get(getCategoryUrl(category))
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    results = soup.find_all('article', {'class': 'wrap-icon'})
+    categoryRecords[category] = {
+        'icons': [],
+        'label': category.title().replace('_', ' ').replace('0', ' + ')
+    }
+    print('Adding icons to category:')
+    for icon in results:
+        iconsCounter += 1
+        iconId = getIconId(icon)
+        categoryRecords[category]['icons'].append(iconId)
+        print('➥ ' + iconId)
+    print('Done ✓')
+
+print('Finished scraping for categories and icons')
+print(f'Parsed {iconsCounter} icons from {len(categoryList)} categories')
+print('Creating category file')
+
+with open("categories.json", "w") as outfile:
+    json.dump(categoryRecords, outfile)
+
+print('Finished ✓')
+driver.close()
